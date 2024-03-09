86440b8cc395390ad9cf66b4bcd3185132d1f941
Anton Prakapniou <prakapniou@gmail.com>
2024-03-09 15:32:28 +0300

    Add user & order services

---
+++ TestTask/TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,18 @@
+﻿using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        public Task<Order> GetOrder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Order>> GetOrders()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TestTask/TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,18 @@
+﻿using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public sealed class UserService : IUserService
+    {
+        public Task<User> GetUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<User>> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
